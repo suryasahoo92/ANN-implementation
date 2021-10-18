6b6d33d5193003a5963b1f80bcaf3085bcbc2919
suryasahoo92 <suryasahoo92@gmail.com>
2021-10-19 00:37:45 +0530

    added save plot function

--- src/training.py
+++ src/training.py
@@ -1,6 +1,6 @@
 from src.utils.common import read_config
 from src.utils.data_mgmt import get_data
-from src.utils.model import create_model, save_model
+from src.utils.model import create_model, save_model, save_plot
 
 import argparse
 import os
@@ -23,15 +23,22 @@ def training(config_path):
 
     history = model.fit(X_train, y_train, epochs=EPOCHS,
                     validation_data=VALIDATION_SET)
+
     artifacts_dir = config["artifacts"]["artifacts_dir"]
     model_dir = config["artifacts"]["model_dr"]
 
     model_dir_path = os.path.join(artifacts_dir, model_dir)
     os.makedirs(model_dir_path, exist_ok=True)
-
     model_name = config["artifacts"]["model_name"]
-
     save_model(model, model_name, model_dir_path)
+
+
+    plot_dir = config["artifacts"]["plots_dir"]
+    plot_dir_path = os.path.join(artifacts_dir, plot_dir)
+    os.makedirs(plot_dir_path, exist_ok=True)
+    plot_name = config["artifacts"]["plot_name"]
+    loss_acc_plot = history.history
+    save_plot(loss_acc_plot, plot_name, plot_dir_path)
 
 if __name__ == '__main__':
     args = argparse.ArgumentParser()
--- src/utils/model.py
+++ src/utils/model.py
@@ -1,6 +1,8 @@
 import tensorflow as tf
 import time
 import os
+import pandas as pd
+import matplotlib.pyplot as plt
 
 
 def create_model(LOSS_FUNCTION, OPTIMIZER, METRICS, NUM_CLASSES):
@@ -29,3 +31,11 @@ def save_model(model, model_name, model_dir):
     path_to_model = os.path.join(model_dir, unique_filename)
     model.save(path_to_model)
 
+def save_plot(loss_acc_plot, plot_name, plot_dir):
+    unique_plot = get_unique_filename(plot_name)
+    path_to_plot = os.path.join(plot_dir, unique_plot)
+
+    pd.DataFrame(loss_acc_plot).plot(figsize=(10, 7))
+    plt.grid(True)
+    #plt.show()
+    plt.savefig(path_to_plot)
